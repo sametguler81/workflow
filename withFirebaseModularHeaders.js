e79fbe4633900780212dc9d532e8b13d8f2f5b04
sametguler81 <sametguler81@yahoo.com.tr>
2026-02-20 13:56:30 +0300

    Add gRPC post_install fix to custom Expo plugin

--- withFirebaseModularHeaders.js
+++ withFirebaseModularHeaders.js
@@ -23,13 +23,32 @@ use_modular_headers!
                         `target 'main' do\n${customBlock}`
                     );
 
-                    // Let's just put it globally at the top if target 'main' wasn't matched properly by Expo
                     if (!contents.includes('# EXPO_FIREBASE_MODULAR_HEADERS')) {
                         contents = customBlock + contents;
                     }
-
-                    fs.writeFileSync(filePath, contents);
                 }
+
+                // Add the gRPC modulemap fix to post_install
+                if (!contents.includes('# GRPC_MODULEMAP_FIX')) {
+                    const grpcFix = `
+    # GRPC_MODULEMAP_FIX
+    installer.pods_project.targets.each do |target|
+      if target.name == 'gRPC-C++'
+        target.build_configurations.each do |config|
+          config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] ||= ['$(inherited)']
+          config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] << 'GRPC_TARGET_CPP'
+        end
+      end
+    end
+`;
+                    // Try to inject at the end of post_install
+                    contents = contents.replace(
+                        /post_install do \|installer\|([\s\S]*?)end\n/g,
+                        (match, p1) => `post_install do |installer|${p1}${grpcFix}    end\n`
+                    );
+                }
+
+                fs.writeFileSync(filePath, contents);
             }
             return config;
         },
