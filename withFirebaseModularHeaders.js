89c887730decac95748713226668fd4ef830512b
sametguler81 <sametguler81@yahoo.com.tr>
2026-02-20 14:02:41 +0300

    Update gRPC workaround

--- withFirebaseModularHeaders.js
+++ withFirebaseModularHeaders.js
@@ -10,14 +10,13 @@ const withFirebaseModularHeaders = (config) => {
             if (fs.existsSync(filePath)) {
                 let contents = fs.readFileSync(filePath, 'utf-8');
 
-                // Check if we've already added our custom block
                 if (!contents.includes('# EXPO_FIREBASE_MODULAR_HEADERS')) {
                     const customBlock = `
 # EXPO_FIREBASE_MODULAR_HEADERS
 $RNFirebaseAsStaticFramework = true
+use_frameworks! :linkage => :static
 use_modular_headers!
 `;
-                    // Find the target runner block and inject right before use_react_native
                     contents = contents.replace(
                         /target 'main' do/g,
                         `target 'main' do\n${customBlock}`
@@ -26,26 +25,6 @@ use_modular_headers!
                     if (!contents.includes('# EXPO_FIREBASE_MODULAR_HEADERS')) {
                         contents = customBlock + contents;
                     }
-                }
-
-                // Add the gRPC modulemap fix to post_install
-                if (!contents.includes('# GRPC_MODULEMAP_FIX')) {
-                    const grpcFix = `
-    # GRPC_MODULEMAP_FIX
-    installer.pods_project.targets.each do |target|
-      if target.name == 'gRPC-C++'
-        target.build_configurations.each do |config|
-          config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] ||= ['$(inherited)']
-          config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] << 'GRPC_TARGET_CPP'
-        end
-      end
-    end
-`;
-                    // Try to inject at the end of post_install
-                    contents = contents.replace(
-                        /post_install do \|installer\|([\s\S]*?)end\n/g,
-                        (match, p1) => `post_install do |installer|${p1}${grpcFix}    end\n`
-                    );
                 }
 
                 fs.writeFileSync(filePath, contents);
